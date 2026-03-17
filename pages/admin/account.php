f780f92c295ae7f2219f7c06dba38d055f77c8d8
John Gabriel Completo <gabrielcompleto3433@gmail.com>
2026-03-17 17:55:13 +0800

    feat added add user

--- pages/admin/account.php
+++ pages/admin/account.php
@@ -27,11 +27,11 @@ verifyUser();
                 <form action="../../api/adminApi.php" method="post" class="w-full mt-5">
                     <div class="mb-5">
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                        <input type="name" id="name" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="<?= $name ?>" required />
+                        <input type="name" id="name" name="name"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" value="<?= $name ?>" required />
                     </div>
                     <div class="mb-5">
-                        <label for="email" class="block mb-2.5 text-sm font-medium ">Email</label>
-                        <input type="email" id="email" name="email" class="bg-gray-400 border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" value="<?= $email ?>" disabled/>
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                        <input type="email" id="email" name="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value="<?= $email ?>" disabled/>
                     </div>
                     <button type="submit" name="changeName" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Save Changes</button>
                 </form>
@@ -42,16 +42,16 @@ verifyUser();
                 <h1 class="text-gray-800 dark:text-gray-200 text-lg">Change Password</h1>
                 <form action="../../api/adminApi.php" method="post" class="w-full mt-5">
                     <div class="mb-5">
-                        <label for="opass" class="block mb-2.5 text-sm font-medium">Old Password</label>
-                        <input type="password" id="opass" name="opass" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="**********" />
+                        <label for="opass" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Old Password</label>
+                        <input type="password" id="opass" name="opass" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="**********" />
                     </div>
                     <div class="mb-5">
-                        <label for="pass1" class="block mb-2.5 text-sm font-medium">New Password</label>
-                        <input type="password" id="pass1" name="pass1" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="**********" />
+                        <label for="pass1" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
+                        <input type="password" id="pass1" name="pass1" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="**********" />
                     </div>
                     <div class="mb-5">
-                        <label for="pass2" class="block mb-2.5 text-sm font-medium">Confirm Password</label>
-                        <input type="password" id="pass2" name="pass2" class="bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full px-3 py-2.5 shadow-xs placeholder:text-body" placeholder="**********" />
+                        <label for="pass2" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+                        <input type="password" id="pass2" name="pass2" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="**********" />
                     </div>
                     
                     <button type="submit" name="changePass" class="text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">Save Changes</button>
